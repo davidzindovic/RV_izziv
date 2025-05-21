633afea9ca61c608f0ab9de015bce019a5873de5
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-05-21 19:48:45 +0200

    Update config_izziv_main.py

--- config_izziv_main.py
+++ config_izziv_main.py
@@ -2,8 +2,8 @@
 
 debug = 0
 debug_prikaz=0              # za prikaz framea z zamudo 1 in za prikaz uvodnih vizualizacij (lučke, polovice slike ipd.)
-debug_outlines=1            # sam za prikaz obrob in shapeov ko najde nekaj (nov pin v 3x3 gridu)
-debug_pins=1                # za podatke o najdenih pinih
+debug_outlines=0            # za prikaz obrob in shapeov ko najde nekaj (nov pin v 3x3 gridu)
+debug_pins=0                # za podatke o najdenih pinih
 debug_sprotno_stanje=0      # za izpis/izris sprotnega stanja pinov
 debug_video=0               # za izpis podatkov o videju (npr. ko ne more dobiti slike)
 debug_prikazi_vsak_frame=0  # zastavica za izris vsakega frame-a
@@ -11,13 +11,14 @@ debug_akcije=0              # zastavica za izpis trenutnega stanja posodic v skl
 debug_false_positive=0      # zastavica za pridobitev true positive tabele za spremembo dogodkov
 debug_anotacija=0           # zastavica za pridobitev primerjave generirane in referenčne anotacije
 debug_robovi_kvadra=0       # zastavica za izris in izpis robov main boarda
+debug_bowl=1                # zastavica za izris najdenih oblik v bowlu in radija bowla
 
-path_do_videjev='C:\\Users\\david\\Desktop\\izziv main\\'
+path_do_videjev='C:\\Users\\David Zindović\\Desktop\\Fax-Mag\\RV\\izziv\\izziv main\\' #vsak \ je potrebno podvojiti v \\
 
 ime_videja="64210323_video_7"
 ime_videja_2="64210323_video_3"
 
-pravilna_anotacija_path='C:\\Users\\david\\Desktop\\izziv main\\rocna_anotacija\\'
+pravilna_anotacija_path='C:\\Users\\David Zindović\\Desktop\\Fax-Mag\\RV\\izziv\\izziv main\\rocna_anotacija\\'
 
 ime_pravilna_anotacija="64210323_video_7"
 ime_pravilna_anotacija_2="64210323_video_3"
