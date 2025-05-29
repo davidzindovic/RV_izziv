750b92170e76b249b6783b46da49f341e2ddc84f
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2025-05-29 23:11:29 +0200

    Update config_izziv_main.py

--- config_izziv_main.py
+++ config_izziv_main.py
@@ -1,13 +1,16 @@
 #-------------ZA UPORABNIKA---------------------------
 
-debug_prikaz=0              # za prikaz framea z zamudo 1 in za prikaz uvodnih vizualizacij (lučke, polovice slike ipd.)
-debug_outlines=0            # za prikaz obrob in shapeov ko najde nekaj (nov pin v 3x3 gridu)
-debug_pins=0                # za podatke o najdenih pinih
-debug_sprotno_stanje=0      # za izpis/izris sprotnega stanja pinov
-debug_video=0               # za izpis podatkov o videju (npr. ko ne more dobiti slike)
+# vrednost posamezne zastavice naj bo 0 ali 1
+
+debug_prikaz=0              # zastavica za prikaz frame-a z zamudo ene slike in za prikaz uvodnih vizualizacij (lučke ipd.)
+debug_outlines=0            # zastavica za prikaz obrob in oblik, ko najde nekaj (nov pin v 3x3 gridu lukenj)
+debug_pins=0                # zastavica za podatke o najdenih pinih
+debug_sprotno_stanje=0      # zastavica za izpis/izris sprotnega stanja pinov, stanja pinov v 3x3 mreži lukenj
+debug_video=0               # zastavica za izpis podatkov o videju (npr. ko ne more dobiti slike)
 debug_prikazi_vsak_frame=0  # zastavica za izris vsakega frame-a
 debug_bowl=0                # zastavica za izris najdenih oblik v bowlu in radija bowla ter izpis podatkov o pinih v bowlu
 debug_visualization_text=0  # zastavica za izris teksta o površini in barvi za najdene pine, ko vizualizira obrobe najdenih oblik
-debug_aktivna_roka=0        # zastavica za izpis aktivne roke
+debug_aktivna_roka=0        # zastavica za izpis aktivne roke (sporočilo: "uporabnik uporablja _____ roko!")
+debug_prehiter_poskus=0     # zastavica za izpis, če je uporabnik prehitro začel s poskusom (sporočilo: "uporabnik je začel PREHITRO poskus" npr.)
 
 #-----------------------------------------------------------------
